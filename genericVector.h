f15fe0b5a3621a51556aa8fe56dddfbbdd301840
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2021-01-04 21:47:39 +0545

    Some bug fixes, memory leak removed, and better formatting

--- genericVector.h
+++ genericVector.h
@@ -1,192 +1,155 @@
 #ifndef GENERICVECTOR_H
 #define GENERICVECTOR_H
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
-struct vector
-{
-    void *arr;
-    unsigned int word_size;
-    unsigned int size;
-    unsigned int capacity;
-    int (*cmp)(const void *,const void *);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct vector {
+  void *arr;
+  unsigned int word_size;
+  unsigned int size;
+  unsigned int capacity;
+  int (*cmp)(const void *, const void *);
 };
 typedef struct vector vector;
 
+// returns the number of elements in the vector
+unsigned int size(vector *vec) { return vec->size; }
 
-//returns the number of elements in the vector
-unsigned int size(vector *vec)
-{
-    return vec->size;
-}
+// returns the capacity of the vector to hold elements
+unsigned int capacity(vector *vec) { return vec->capacity; }
 
+// tells you if the vector is empty
+int isempty(vector *vec) { return (size(vec) == 0) ? 1 : 0; }
 
-//returns the capacity of the vector to hold elements
-unsigned int capacity(vector *vec)
-{
-    return vec->capacity;
-}
-
-
-//tells you if the vector is empty
-int isempty(vector *vec)
-{
-    return (size(vec)==0)?1:0;
-}
-
-//pushes the data to the end of the array. Resizes if needed.
-void push_back(vector *vec,void *data)
-{
-  if(size(vec)<capacity(vec))
-    {
-      char *ptr=(char *)vec->arr;
-      ptr+=(size(vec)*(vec->word_size));
-      memcpy((void *)ptr,(const void *) data,vec->word_size);
-      vec->size++;
+// pushes the data to the end of the array. Resizes if needed.
+void push_back(vector *vec, void *data) {
+  if (size(vec) < capacity(vec)) {
+    char *ptr = (char *)vec->arr;
+    ptr += (size(vec) * (vec->word_size));
+    memcpy((void *)ptr, (const void *)data, vec->word_size);
+    vec->size++;
+  } else {
+    if (size(vec) == 0) {
+      vec->arr = calloc(2, vec->word_size);
+      vec->capacity = 2;
+    } else {
+      void *tmp = vec->arr;
+      vec->arr = calloc(2 * capacity(vec), vec->word_size);
+      vec->capacity = 2 * capacity(vec);
+      memcpy(vec->arr, tmp, size(vec) * vec->word_size);
+      free(tmp);
     }
-  else{
-      if(size(vec)==0)
-        {
-          vec->arr=calloc(2,vec->word_size);
-          vec->capacity=2;
-        }
-      else
-        {
-          vec->arr=realloc(vec->arr,2*capacity(vec)*vec->word_size);
-          vec->capacity=2*capacity(vec);
-        }
-      push_back(vec,data);
-    }
-
+    push_back(vec, data);
+  }
 }
 
-
-
-//pops the element at the end of the array
-//The number will be there as is. It wont be removed but will be overwritten next time you do
-//push_back
-void pop_back(vector  *vec)
-{
-    if(size(vec)!=0)
-      {
-        vec->size-=1;
-      }
+// pops the element at the end of the array
+// The number will be there as is. It wont be removed but will be overwritten
+// next time you do push_back
+void pop_back(vector *vec) {
+  if (size(vec) != 0) {
+    vec->size -= 1;
+  }
 }
 
-
-
-
-//Vector initializer
-vector* Vector(int word_size,int (*compare)(const void *,const void *))
-{
-  vector *vec=(vector *)calloc(1,sizeof (vector));
-  vec->cmp=compare;
-  vec->word_size=word_size;
-  vec->size=0;
-  vec->size=0;
+// Vector initializer
+vector *Vector(unsigned int word_size,
+               int (*compare)(const void *, const void *)) {
+  vector *vec = (vector *)calloc(1, sizeof(vector));
+  vec->cmp = compare;
+  vec->word_size = word_size;
+  vec->size = 0;
+  vec->capacity = 0;
 
   return vec;
 }
 
-//Reserves the needed amount of memory and returns a heap allocated vector
-vector *initAndReserve(int word_size,int(*compare)(const void *,const void *),unsigned int capacity)
-{
-    vector *vec=Vector(word_size,compare);
-    vec->arr=calloc(capacity,word_size);
-    vec->capacity=capacity;
-    return vec;
+// Reserves the needed amount of memory and returns a heap allocated vector
+vector *initAndReserve(unsigned int word_size,
+                       int (*compare)(const void *, const void *),
+                       unsigned int capacity) {
+  vector *vec = Vector(word_size, compare);
+  vec->arr = calloc(capacity, word_size);
+  vec->capacity = capacity;
+  return vec;
 }
 
-
-
-//deletes the heap allocated memory
-void delete(vector *vec)
-{
-  if(vec->arr!=NULL){
+// deletes the heap allocated memory
+void delete (vector *vec) {
+  if (vec->arr != NULL) {
     free(vec->arr);
-    vec->arr=NULL;
-    }
+    vec->arr = NULL;
+  }
 }
 
-
-void *get(vector *vec,unsigned int index)
-{
-  return (void *)((char *)vec->arr +(index *  vec->word_size));
-
+void *get(vector *vec, unsigned int index) {
+  return (void *)((char *)vec->arr + (index * vec->word_size));
 }
 
-void *set(vector *vec,void * data,int index )
-{
-  if(index<capacity(vec))
-    {
-        void *ptr=(void *)((char *) vec->arr + (index * (vec->word_size)));
-        memcpy(ptr,(const void *) data,vec->word_size);
-        if(index>=vec->size)
-          vec->size=(index+1);
+void *set(vector *vec, void *data, unsigned int index) {
+  if (index < capacity(vec)) {
+    void *ptr = (void *)((char *)vec->arr + (index * (vec->word_size)));
+    memcpy(ptr, (const void *)data, vec->word_size);
+    if (index >= vec->size)
+      vec->size = (index + 1);
 
-        return ptr;
-    }
+    return ptr;
+  }
   return NULL;
 }
 
-//shrinks the vector to the size of the vector. No unwanted storage used
-void shrinkToFit(vector *vec)
-{
-  if(size(vec)!=0)
-    {
-      vec=realloc(vec->arr,vec->size * vec->word_size);
-      vec->capacity=vec->size;
+// shrinks the vector to the size of the vector. No unwanted storage used
+void shrinkToFit(vector *vec) {
+  if (size(vec) != 0) {
+    void *tmp = vec->arr;
+    vec->arr = calloc(vec->size, vec->word_size);
+    memcpy(vec->arr, tmp, size(vec) * vec->word_size);
+    free(tmp);
+    vec->capacity = vec->size;
+  }
+}
+
+// Insert an element at arbitrary position
+void insert(vector *vec, void *elemAddr, unsigned int position) {
+  unsigned int len = size(vec);
+  if (len == position) {
+    push_back(vec, elemAddr);
+    return;
+  } else {
+    push_back(vec, (char *)vec->arr + (len - 1) * vec->word_size);
+    memmove((void *)((char *)vec->arr + (position + 1) * vec->word_size),
+            (void *)((char *)vec->arr + position * vec->word_size),
+            (len - position - 1) * vec->word_size);
+    set(vec, elemAddr, position);
+    return;
+  }
+}
+
+// Delete an element from an arbitrary position
+void removeAt(vector *vec, unsigned int position) {
+
+  int len = size(vec);
+  if (position == len - 1) {
+    pop_back(vec);
+  } else {
+    for (unsigned int i = position + 1; i < size(vec); i++) {
+      set(vec, get(vec, i), i - 1);
     }
+    (vec->size)--;
+  }
 }
 
-
-//Insert an element at arbitrary position
-void insert(vector *vec, void *elemAddr,unsigned int position)
-{
-		unsigned int len=sizeof(vec);
-		if(len==position){
-			push_back(vec,elemAddr);
-			return;
-		}
-		else{
-				int arr[len-position];
-				unsigned int lentmp=len-position;
-				memcpy(arr,(char *)(vec->arr)+position*vec->word_size,lentmp*(vec->word_size));
-				set(vec,elemAddr,position);
-				for(unsigned int i=0;i<(lentmp-1);++i)
-						set(vec,(void *)(arr+i),position+i+1);
-				push_back(vec,(void *)(arr+lentmp-1));
-				return;
-		}
-		
+void printAll(vector *vec, void (*printFunc)(const void *),
+              char printNewLineAtTheEnd) {
+  for (unsigned int i = 0; i < size(vec); i++) {
+    printFunc(get(vec, i));
+  }
+  if (printNewLineAtTheEnd) {
+    printf("\n");
+  }
 }
 
-//Delete an element from an arbitrary position
-void removeAt(vector *vec,unsigned int position)
-{
-
-		int len=sizeof(vec);
-		int tmpLen=len-position-1;
-		if(position==len-1){
-				pop_back(vec);
-		}
-		else{
-				void *dest=(void *)((char *)vec->arr+position*vec->word_size);
-				void *src=(void *)((char  *)dest+vec->word_size);
-				memcpy(dest,src,tmpLen*vec->word_size);
-				(vec->size)--;
-		}
-}
-
-
-
-
-//uses the built in qsort to sort the generic vector
-void sort(vector *vec)
-{
-  qsort(vec->arr,size(vec),vec->word_size,vec->cmp);
-}
-
-
+// uses the built in qsort to sort the generic vector
+void sort(vector *vec) { qsort(vec->arr, size(vec), vec->word_size, vec->cmp); }
 
 #endif // GENERICVECTOR_H
--- main.c
+++ main.c
@@ -1,23 +1,29 @@
+#include "genericVector.h"
 #include <stdio.h>
-#include"genericVector.h"
 
-int cmp(const void *a,const void *b)
-{
-  return *(int *)a - *(int *)b;
-}
-int main()
-{
-  vector *vec=Vector(sizeof (int),cmp);
-  for(int i=5;i>=1;i--)
-    push_back(vec,(void *)&i);
+void printInt(const void *val) { printf("%d ", *(int *)val); }
+
+int cmp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+int main() {
+  vector *vec = Vector(sizeof(int), cmp);
+  for (int i = 5; i >= 1; i--)
+    push_back(vec, (void *)&i);
   sort(vec);
-  for(int i=0;i<5;i++)
-    printf("%d\n",*(int *)get(vec,i));
- 
-  removeAt(vec,2);
-  for(int i=0;i<=3;i++)
-    printf("%d\n",*(int *)get(vec,i));
-  delete(vec);
+  for (int i = 0; i < 5; i++)
+    printf("%d\n", *(int *)get(vec, i));
+
+  printf("\n\n");
+
+  removeAt(vec, 2);
+  for (int i = 0; i < size(vec); i++)
+    printf("%d\n", *(int *)get(vec, i));
+  int storeThis = 3;
+  insert(vec, &storeThis, 2);
+  insert(vec, &storeThis, 2);
+  insert(vec, &storeThis, 2);
+  insert(vec, &storeThis, 2);
+  printAll(vec, printInt, 1);
+  delete (vec);
   free(vec);
   return 0;
 }
